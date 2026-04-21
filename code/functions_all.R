8f7d96766af19aed8a644c4479d5f58375c637cb
sfaridan <stefan.faridani@gmail.com>
2026-04-21 16:01:25 -0400

    corrected pval

--- code/functions_all.R
+++ code/functions_all.R
@@ -350,6 +350,7 @@ run_sims<- function(parms,sim_file_prefix="sim_parms_"){
         boot95[sim] <- sim_results$boot95
         
         rejects[sim] <- sim_results$pval <= .05  #1*(resids[sim]>boot95[sim]+epsilon_U+nu_resid) 
+        print("Pval =", sim_results$pval)
       }
       
       
@@ -477,7 +478,7 @@ run_test<- function(data,numcoeffs,sigma_Y=1,shift=1.96,L=6.5,numgrid=3000,boots
     print(paste0("boot ", b, " of ", boots))
     print(c( orig_resid,quantile(resids_boot[1:b],0.95),quantile(resids_boot[1:b],0.90)))
   }
-  pval             <- mean(orig_resid > resids_boot+epsilon_U)
+  pval             <- mean(orig_resid < resids_boot+epsilon_U)
   breakdown        <- orig_resid - quantile(resids_boot,0.95)-epsilon_U
   
   return(list(n=length(data$t),articles=length(unique(data$title)),resid=orig_resid,epsilon_U=epsilon_U,boot95=quantile(resids_boot,0.95),pval=pval,breakdown=breakdown,projpoint =projpoint))
